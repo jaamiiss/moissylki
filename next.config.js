8b9be13b34b3652ce37938e6893882e28bda7d9d
James Magallanes <fridaysyckness@gmail.com>
2023-02-17 14:57:39 +0800

    restruc
    + app directory

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['images.unsplash.com', 'source.unsplash.com'],
+  }
 }
 
 module.exports = nextConfig
