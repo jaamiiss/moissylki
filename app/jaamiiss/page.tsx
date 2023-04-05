330c8dbc8fc12e02b672d96243016c71879b5852
James Magallanes <fridaysyckness@gmail.com>
2023-04-05 12:14:00 +0800

    Create page.tsx

---
+++ app/jaamiiss/page.tsx
@@ -0,0 +1,111 @@
+import Image from 'next/image';
+import '@/styles/globals.css';
+import { get } from '@vercel/edge-config';
+import { redirect } from 'next/navigation';
+
+export const dynamic = 'force-dynamic',
+  runtime = 'edge';
+  
+function LinkCard({ href, title, image }: {
+  href: string;
+  title: string;
+  image?: string;
+}) {
+  return (
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="flex items-center p-1 w-full rounded-md hover:scale-105 transition-all border border-gray-300 bg-gray-100 mb-3 max-w-3xl">
+        <div className='flex text-center w-full'>
+          <div className='w-10 h-10'>
+            {image && (
+            <Image 
+              className='rounded-sm'
+              alt={title}
+              src={image}
+              width={40}
+              height={40}
+            />
+            )}
+          </div>
+          <h2 className='flex justify-center items-center font-semibold w-full text-center text-gray-700 -ml-10'>{title}</h2>                    
+          </div>
+      </a>
+  )
+}
+
+function Socials({ href, title, image }: {
+  href: string;
+  title: string;
+  image?: string;
+}) {
+  return (
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer">
+        <div className='flex text-center w-full mt-4' >
+          <div className='w-10 h-10'>
+            {image && (
+            <Image 
+              className='rounded-sm fill-white-500'
+              alt={title}
+              src={image}
+              width={40}
+              height={40}
+            />
+            )}
+          </div>                  
+        </div>
+      </a>
+  )
+}
+
+interface Data {
+  name: string;
+  avatar: string;
+  links: Link[];
+  socials: Social[];
+}
+
+interface Link {
+  href: string;
+  title: string;
+  image?: string;
+}
+
+interface Social {
+  href: string;
+  title: string;
+  image?: string;
+}
+
+
+export default async function Home() {
+  const data: Data | undefined = await get('mylinks');
+
+  if (!data) {
+    // redirect to local data
+    redirect('https://moissylki.vercel.app/');
+  }
+
+  return (
+    <div className='flex items-center flex-col mx-auto w-full justify-center mt-16 px-8'>
+      <Image 
+        className='rounded-full'
+        alt={data.name}
+        src={data.avatar}
+        width={120}
+        height={120}
+      />
+      <h1 className='font-bold mt-4 mb-8 text-xl'>{data.name}</h1>
+      {data.links.map((link) => (
+        <LinkCard key={link.href} {...link} />
+      ))}
+      {data.socials.map((link) => (
+        <Socials key={link.href} {...link} />
+      ))}
+    </div>
+  );
+}
