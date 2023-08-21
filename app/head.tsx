68c27c27f1e227da2019f5c706d48279dc236641
James Magallanes <fridaysyckness@gmail.com>
2023-08-21 18:12:52 +0800

    Update packages
    Data and meta

--- app/head.tsx
+++ app/head.tsx
@@ -5,6 +5,7 @@ export default function Head() {
             <meta name="description" content="Next.js + Tailwind CSS"/>
             <link rel="icon" href="/favicon.ico" />
             <meta name="viewport" content="width=device-width.initial-scale=1"/>  
+            <meta name="robots" content="noarchive"/>
         </>
     );
 }
